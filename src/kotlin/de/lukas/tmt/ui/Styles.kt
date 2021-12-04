effd1aaca66ea9f6edd47e0b250dcbb1bf5e2c3d
Lukas Eisenhauer <leisenhau@gmail.com>
2021-12-04 17:31:09 +0100

    nicer task list layout

--- src/kotlin/de/lukas/tmt/ui/Styles.kt
+++ src/kotlin/de/lukas/tmt/ui/Styles.kt
@@ -34,9 +34,102 @@
 
 package de.lukas.tmt.ui
 
-import tornadofx.Stylesheet
+import javafx.geometry.Pos
+import tornadofx.*
 
 class Styles : Stylesheet() {
     init {
+        taskCard {
+            textFill = textColor
+            borderColor += box(border)
+            borderWidth += box(2.px)
+            padding = box(10.px)
+        }
+
+        text {
+            fill = foreground
+        }
+
+        Stylesheet.root {
+            backgroundColor += background
+            stroke = textColor
+        }
+
+        heading {
+            fontSize = 3.em
+            alignment = Pos.BASELINE_CENTER
+        }
+
+        jfxTabPane {
+            tabHeaderArea {
+                tabHeaderBackground {
+                    backgroundColor += secondBackground
+                    text {
+                        fill = foreground
+                    }
+                }
+            }
+        }
+
+        tabSelectedLine {
+            backgroundColor += orange
+            text {
+                fill = foreground
+            }
+        }
+
+        tab {
+            padding = box(10.px)
+        }
+
+        tabContentArea {
+            padding = box(10.px)
+        }
+    }
+
+    companion object {
+        val taskCard by cssclass()
+        val heading by cssclass()
+
+        val jfxTabPane by cssclass("jfx-tab-pane")
+        val tabSelectedLine by cssclass("tab-selected-line")
+
+        // material oceanic
+        val background = c("#263238")
+        val foreground = c("#B0BEC5")
+        val textColor = c("#607D8B")
+        val selectionBackground = c("#546E7A")
+        val selectionForeground = c("#FFFFFF")
+        val buttons = c("#2E3C43")
+        val secondBackground = c("#32424A")
+        val disabled = c("#415967")
+        val contrast = c("#1E272C")
+        val active = c("#314549")
+        val border = c("#2A373E")
+        val highlight = c("#425B67")
+        val tree = c("#546E7A70")
+        val notifications = c("#1E272C")
+        val accent = c("#009688")
+        val excluded = c("#2E3C43")
+        val green = c("#c3e88d")
+        val yellow = c("#ffcb6b")
+        val blue = c("#82aaff")
+        val red = c("#f07178")
+        val purple = c("#c792ea")
+        val orange = c("#f78c6c")
+        val cyan = c("#89ddff")
+        val gray = c("#546e7a")
+        val error = c("#ff5370")
+        val comments = c("#546e7a")
+        val variables = c("#eeffff")
+        val links = c("#80cbc4")
+        val functions = c("#82aaff")
+        val keywords = c("#c792ea")
+        val tags = c("#f07178")
+        val strings = c("#c3e88d")
+        val operators = c("#89ddff")
+        val attributes = c("#ffcb6b")
+        val numbers = c("#f78c6c")
+        val parameters = c("#f78c6c")
     }
 }
