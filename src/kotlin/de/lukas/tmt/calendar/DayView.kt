ea74e73fbaf8fc8fb6f085dd5199deb0e769133e
Lukas Eisenhauer <leisenhau@gmail.com>
2021-12-11 18:10:25 +0100

    calendar: show relevant days

---
+++ src/kotlin/de/lukas/tmt/calendar/DayView.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 Lukas Eisenhauer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.lukas.tmt.calendar
+
+import de.lukas.tmt.task.Task
+import de.lukas.tmt.ui.Styles
+import tornadofx.Fragment
+import tornadofx.addClass
+import tornadofx.label
+import tornadofx.vbox
+import java.sql.Date
+
+class DayView(private val day: Long) : Fragment() {
+    override val root = vbox {
+        addClass(Styles.root)
+        label(Date(day * Task.MILLISECONDS_PER_DAY).toString())
+    }
+}
